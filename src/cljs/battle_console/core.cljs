0286ed6385a6073d2f907d567987f88ec4c4ba18
Pedro Santos <donbonifacio@gmail.com>
2014-11-23 09:10:15 +0000

    Secretary working

--- src/cljs/battle_console/core.cljs
+++ src/cljs/battle_console/core.cljs
@@ -1,15 +1,51 @@
 (ns battle-console.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [goog.events :as events]
+            [secretary.core :as secretary :refer-macros [defroute]])
+  (:import goog.History
+             goog.history.EventType)
+  )
 
 (defonce app-state (atom {:text "Hello Chestnut!"}))
 
-(defn main []
+(secretary/set-config! :prefix "#")
+
+(defroute "/" {:as params}
+  (js/console.log "GET /")
+  )
+
+(defroute "/help" {:as params}
+  (js/console.log "GET /help")
+  )
+
+;; Catch all
+(defroute "*" []
+  (js/console.log "ALL"))
+
+
+;; Quick and dirty history configuration.
+(let [h (History.)]
+    (goog.events/listen h EventType.NAVIGATE #(secretary/dispatch! (.-token %)))
+      (doto h
+            (.setEnabled true)))
+
+(defn h1 [app owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/h1 nil (:text app)))))
+
+(defn h2 [app owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/h2 nil (:text app)))))
+
+(defn main
+  []
+  (secretary/dispatch! "/")
   (om/root
-    (fn [app owner]
-      (reify
-        om/IRender
-        (render [_]
-          (dom/h1 nil (:text app)))))
+    h1
     app-state
     {:target (. js/document (getElementById "app"))}))
