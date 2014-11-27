67c87472198255c646683811bb096886ec1564bd
Pedro Santos <donbonifacio@gmail.com>
2014-11-27 16:23:37 +0000

    Make games handler load games

--- src/cljs/battle_console/games.cljs
+++ src/cljs/battle_console/games.cljs
@@ -5,10 +5,26 @@
             [om.dom :as dom :include-macros true]
             [secretary.core :as secretary :refer-macros [defroute]]))
 
+(defn- games-loaded
+  "After games were loaded"
+  [data]
+  (state/set-state :loading-games nil)
+  (state/set-state :games-data data))
+
+(defn- error-loading
+  "Error loading games"
+  []
+  (state/set-state :loading-games "Error!"))
+
 (defn handler
   "Handles the root page and asks for the token"
   []
-  (state/set-page :games))
+  (state/set-page :games)
+  (state/set-state :loading-games "Loading games...")
+  (let [token (state/get-state :token)
+        url (str "http://api.orionsbelt.eu/player/latest-games?token=" token)]
+    (GET url {:handler games-loaded
+              :error-handler error-loading})))
 
 (defn- get-master-css
   "Getst the master CSS"
@@ -31,15 +47,18 @@
   "Renders the index page"
   [app owner]
   (let [data (state/get-state :games-data)]
-    (om/component (dom/div #js {:className (get-master-css)}
-                    (dom/h1 nil (str (count (state/get-state :games-data)) " games available"))
-                    (apply dom/table #js {:className "table table-striped table-hover "}
-                           (dom/tr nil
-                                   (dom/th nil "Game ID")
-                                   (dom/th nil "State")
-                                   (dom/th nil "P1")
-                                   (dom/th nil "P2"))
-                          (map game-row data))))))
+    (om/component
+      (if-let [msg (state/get-state :loading-games)]
+        (dom/p nil msg)
+        (dom/div #js {:className (get-master-css)}
+                      (dom/h1 nil (str (count (state/get-state :games-data)) " games available"))
+                      (apply dom/table #js {:className "table table-striped table-hover "}
+                             (dom/tr nil
+                                     (dom/th nil "Game ID")
+                                     (dom/th nil "State")
+                                     (dom/th nil "P1")
+                                     (dom/th nil "P2"))
+                            (map game-row data)))))))
 
 (defn- register-renderer
   "Register an Om renderer for this page"
