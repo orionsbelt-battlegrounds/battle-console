a07418acc4837af13a3968af2511c680b72be21e
Pedro Santos <donbonifacio@gmail.com>
2014-11-29 18:19:13 +0000

    Show stash

--- src/cljs/battle_console/game.cljs
+++ src/cljs/battle_console/game.cljs
@@ -44,6 +44,27 @@
             (dom/td nil (str (get-in game ["p1" "name"]) " vs " (get-in game ["p2" "name"])))
             (dom/td nil (get game "_id"))))))))
 
+(defn- get-stash
+  "Gets the current stash"
+  [game]
+  (get-in game ["battle" "stash" "p1"]))
+
+(defn- game-stash
+  "Shows the current stash if available"
+  [game]
+  (dom/div #js {:className "row"}
+    (dom/div #js {:className "col-lg-4"}
+      (dom/div #js {:className "bs-component"}
+        (apply dom/table #js {:className "table table-striped table-hover"}
+          (dom/caption nil "Stash")
+          (dom/tr nil
+            (dom/th nil "Unit")
+            (dom/th nil "Quantity"))
+          (for [[unit quantity] (get-stash game)]
+            (dom/tr nil
+              (dom/td nil unit)
+              (dom/td nil quantity))))))))
+
 (defn- render-game
   "Renders the index page"
   [state owner]
@@ -52,7 +73,8 @@
       (if-let [msg (state/get-state :loading-game)]
         (dom/div nil msg)
         (dom/div nil
-                 (game-header (state :game-data))))
+                 (game-header (state :game-data))
+                 (game-stash (state :game-data))))
       (dom/div #js {:className "hide"}))))
 
 (defn- register-renderer
